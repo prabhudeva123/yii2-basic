a6ec06b9d67d52f7fd9fab4e802c33a960b5fe6c
prabhudeva123 <prabudevarajan123@gmail.com>
2016-04-28 17:30:41 +0530

    client
    updated commit

--- smp1-yii2/web/index-test.php
+++ smp1-yii2/web/index-test.php
@@ -12,5 +12,5 @@ require(__DIR__ . '/../vendor/autoload.php');
 require(__DIR__ . '/../vendor/yiisoft/yii2/Yii.php');
 
 $config = require(__DIR__ . '/../tests/codeception/config/acceptance.php');
-
+echo $config;
 (new yii\web\Application($config))->run();
